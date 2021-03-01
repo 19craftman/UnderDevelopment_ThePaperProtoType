f8f0e868c9a95134dcc3dbdcdf1dd8f168ed7289
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2021-03-01 16:28:59 -0500

    Camera movement
    made script that allows camera movement on hover over. implemented in Emma's scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CameraMove cameraMove;
+    enum Direction {left, right, up, down };
+    [SerializeField] private Direction dir;
+    private float hoverTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hoverTime = 0f;
+    }
+
+    private void OnMouseOver()
+    {
+        hoverTime += Time.deltaTime;
+
+        if(hoverTime >=.5f)
+        {
+            hoverTime = 0f;
+            if (dir == Direction.left)
+            {
+                cameraMove.left();
+            }
+            else if (dir == Direction.right)
+            {
+                cameraMove.right();
+            }
+            else if (dir == Direction.up)
+            {
+                cameraMove.up();
+            }
+            else if (dir == Direction.down)
+            {
+                cameraMove.down();
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        hoverTime = 0f;
+    }
+}
---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private Vector3 locations = new Vector3(0,-10,-10);
+    private float moveby;
+    private bool moving;
+    private float moveTime = 2f;
+
+    void Start()
+    {
+        moveby = GetComponent<Camera>().orthographicSize*2f;
+        transform.position = locations;
+        moving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void left()
+    {
+        if(locations.x !=0 && !moving)
+        {
+            locations.x -= moveby;
+            StartCoroutine(move());
+        }
+    }
+
+    public void right()
+    {
+        if (locations.x == 0 && !moving)
+        {
+            locations.x += moveby;
+            StartCoroutine(move());
+        }
+    }
+
+    public void up()
+    {
+        if (locations.y!=0 && !moving)
+        {
+            locations.y += moveby;
+            StartCoroutine(move());
+        }
+    }
+
+    public void down()
+    {
+        if (locations.y!=-20 && !moving)
+        {
+            locations.y -= moveby;
+            StartCoroutine( move());
+        }
+    }
+
+
+    IEnumerator move()
+    {
+        moving = true;
+        //Vector3 startPos = transform.position;
+        float elapsedTime = 0;
+
+        while(elapsedTime<moveTime)
+        {
+            transform.position = Vector3.Lerp(transform.position, locations, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        moving = false;
+    }
+
+}
