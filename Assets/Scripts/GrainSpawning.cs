4a1a7c6d6123beda299b8a622cd0561b3c21478b
19craftman <47764907+19craftman@users.noreply.github.com>
2021-03-02 22:02:41 -0500

    Grain is added

---
+++ Assets/Scripts/GrainSpawning.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrainSpawning : MonoBehaviour
+{
+    public GameObject Grain;
+    public List<GameObject> numberOfGain = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+    public void OnMouseDown()
+    {
+        if (numberOfGain.Count < 3)
+        {
+            GameObject newGrain = Instantiate(Grain, transform.position, transform.rotation);
+            numberOfGain.Add(newGrain);
+        }
+    }
+}
