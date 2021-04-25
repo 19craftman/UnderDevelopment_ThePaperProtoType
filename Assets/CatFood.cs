17adc7d504289e562dcab6c7f228913f91ad62b9
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2021-04-25 17:48:25 -0400

    Cat food Script

---
+++ Assets/CatFood.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatFood : MonoBehaviour
+{
+    private bool dragging;
+    private bool colliding;
+    private Sprite full;
+    private Sprite empty;
+    [SerializeField] private GameObject bowl;
+
+    private void Start()
+    {
+        dragging = false;
+        colliding = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        dragging = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.Equals(bowl))
+        {
+            colliding = true;
+        }
+    }
+}
