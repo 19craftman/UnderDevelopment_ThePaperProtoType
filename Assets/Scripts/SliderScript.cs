30103c45f530bfa9e6576e2ec9d7fde37b9bbf8f
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2021-05-02 22:11:33 -0400

    Added UI(not Complete)

---
+++ Assets/Scripts/SliderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+    [SerializeField] private Slider s;
+    [SerializeField] private Image i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<Slider>();
+    }
+
+   public void changeImage()
+    {
+        i.fillAmount = s.value / s.maxValue;
+    }
+}
