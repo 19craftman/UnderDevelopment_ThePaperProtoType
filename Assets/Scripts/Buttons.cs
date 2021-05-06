403c255f622815100f7882d0e63d1ff0f76deaad
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2021-05-06 17:59:44 -0400

    Update Buttons.cs

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -13,7 +13,7 @@ public class Buttons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape) && !GameState.endGame)
+        if(Input.GetKeyDown(KeyCode.Escape) && !GameState.endGame && GameState.titleScreenComplete)
         {
             pauseMenu.SetActive(!pauseMenu.activeInHierarchy);
             if(pauseMenu.activeInHierarchy)
